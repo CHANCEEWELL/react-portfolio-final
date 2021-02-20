60afd40435687ad80b0e678bfe7ef7c35264e180
Chance Ewell <chance.shane.ewell@gmail.com>
2021-02-20 00:58:36 -0700

    adding contact.js and php try

---
+++ src/components/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+import PHP from 'index.php';
+import Footer from '../Footer';
+
+
+export default function Portfolio() {
+  return (
+  <>
+    <h1> <br></br> Contact 
+    </h1>
+    <PHP />
+    <Footer />
+  </>
+  );
+}
