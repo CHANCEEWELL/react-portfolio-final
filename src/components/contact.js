219e431d3a09a2fc687c2dd8127c5280a167e62b
Chance Ewell <chance.shane.ewell@gmail.com>
2021-02-20 00:40:49 -0700

    added contact.js

---
+++ src/components/contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+import 'index.php';
+import Footer from '../Footer';
+
+
+export default function Portfolio() {
+  return (
+  <>
+    <h1> <br></br> Contact 
+    </h1>
+    <index.php />
+    <Footer />
+  </>
+  );
+}
