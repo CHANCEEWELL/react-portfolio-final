0cbe864f87159bf5024c755c83241b95cae01f19
Chance Ewell <chance.shane.ewell@gmail.com>
2021-02-20 00:37:48 -0700

    added contact page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Portfolio from './components/pages/Portfolio';
-
+import Portfolio from './components/pages/contact';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/portfolio' component={Portfolio} />
+          <Route path='/Contact' component={contact} />
         </Switch>
       </Router>
     </>
