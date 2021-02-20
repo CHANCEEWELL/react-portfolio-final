4264d0165356caf5ef5316994aef0ac2f68de27f
Chance Ewell <chance.shane.ewell@gmail.com>
2021-02-20 00:51:42 -0700

    added contact information

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import '../../App.css';
-import 'index.php';
 import Footer from '../Footer';
 
 
 export default function Portfolio() {
   return (
   <>
-    <h1> <br></br> Contact 
+    <h1> Contact 
     </h1>
-    <index.php />
+    <br></br>
+    <div className='hero-container'>
+      <embed type="image/jpg" src="profile.jpeg" width="300" height="200"></embed>
+    <p>Phone: 435-256-7527 </p>
+    <br></br>
+    <p>Email: Chance.shane.ewell@gmail.com </p>
     <Footer />
   </>
   );
